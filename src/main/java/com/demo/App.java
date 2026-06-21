6c6d256c07dd31160b2be62f698f74e9fd56eab4
Nikita Hawaldar <nikitahawaldar42@gmail.com>
2026-06-22 00:53:59 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,7 +1,12 @@
 package com.demo;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         System.out.println("Register App Started Successfully");
+
+        while (true) {
+            Thread.sleep(10000);
+        }
     }
 }
