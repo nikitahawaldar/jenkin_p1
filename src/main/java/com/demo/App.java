88a82b1326003dc56757057b40d9937abe5b6d99
Nikita Hawaldar <nikitahawaldar42@gmail.com>
2026-06-22 01:17:50 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,12 +1,8 @@
-package com.demo;
+@RestController
+public class AppController {
 
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        System.out.println("Register App Started Successfully");
-
-        while (true) {
-            Thread.sleep(10000);
-        }
+    @GetMapping("/")
+    public String home() {
+        return "Jenkins + Docker + ArgoCD + EKS Success!";
     }
 }
