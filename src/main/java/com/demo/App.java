5b391915176ac1625448c5b8d95062178f81dc8e
Nikita Hawaldar <nikitahawaldar42@gmail.com>
2026-06-21 23:28:03 +0530

    Create App.java

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Register App Started Successfully");
+
+        while(true){
+            Thread.sleep(10000);
+        }
+    }
+}
