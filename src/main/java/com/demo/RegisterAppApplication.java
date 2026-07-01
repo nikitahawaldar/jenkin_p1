287a61da35d2fa6b20167439aac465d85ebb1a40
Nikita Hawaldar <nikitahawaldar42@gmail.com>
2026-07-01 14:13:08 +0530

    Update RegisterAppApplication.java

--- src/main/java/com/demo/RegisterAppApplication.java
+++ src/main/java/com/demo/RegisterAppApplication.java
@@ -1,1 +1,12 @@
+package com.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterAppApplication.class, args);
+    }
+}
